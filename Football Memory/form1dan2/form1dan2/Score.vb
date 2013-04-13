222f93b850ae1a9e7d747f70079499ee9f7c85da
axelpw <aaxelpw@gmail.com>
2013-04-13 19:15:17 +0700

    TUGAS GAME VISUAL BASIC (Kelompok 21)
    Tugas ini untuk memenuhi matakuliah yang dibimbing oleh pak Jauharul
    Fuad

---
+++ Football Memory/form1dan2/form1dan2/Score.vb
@@ -0,0 +1,7 @@
+﻿Public Class Score
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Hide()
+        choose.Show()
+    End Sub
+End Class
